52bfb5766f5ecabde82e2aeba75478e3b40fe450
Eugenia Matto <98404484+EugeniaMatto@users.noreply.github.com>
2022-08-22 20:29:43 -0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,6 @@
 # set up your client SSH configuration file so that you can
 # connect to a server without typing a password
+# use: sudo puppet apply 100-puppet_ssh_config.pp
 
 file_line { 'Identity file':
   path   => '/etc/ssh/ssh_config',
